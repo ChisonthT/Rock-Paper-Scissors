df61abcdca5506074b6eb7c7c2a117763e6c997a
Chisonth <1533494+ChisonthT@users.noreply.github.com>
2024-03-31 15:32:59 -0400

    Created the files for Rock paper scissors page. Added the rock paper scissors logic.

---
+++ script.js
@@ -0,0 +1,48 @@
+function getcomputerchoice(){
+    let com_choice = Math.floor(Math.random() * 3);
+    let result;
+    switch(com_choice){
+        case 0:
+            result = 'Rock';
+            break;
+        case 1:
+            result = 'Paper';
+            break;
+        default:
+            result = 'Scissor';
+    }
+
+    return result; 
+}
+
+function getplayerselection(){
+    let result = prompt("Please enter either rock, paper, scissors: ", "rock");
+    return result;
+}
+
+function playround(player_selection,computer_selection){
+    p_s = player_selection.toUpperCase();
+    c_s = computer_selection.toUpperCase();
+
+    if (p_s == c_s){
+        console.log("It's a draw!");
+    }else if ((p_s == 'ROCK') && (c_s == 'PAPER')){
+        console.log("You LOSE!! Paper beats Rock.");
+    }else if ((p_s == 'PAPER') && (c_s == 'SCISSOR')){
+        console.log("You LOSE!! Scissor beats paper.");
+    }else if ((p_s == 'SCISSOR') && (c_s == 'ROCK')){
+        console.log("You LOSE!! Scissor beats Rock.");
+    }else if ((p_s == 'ROCK') && (c_s == 'SCISSOR')){
+        console.log("You WIN!! Rock beats scissor.");
+    }else if ((p_s == 'SCISSOR') && (c_s == 'PAPER')){
+        console.log("You WIN!! Scissor beats paper.");
+    }else{
+        console.log("You WIN!! Paper beats rock.");
+    }
+}
+
+
+let player_selection = getplayerselection();
+let computer_selection = getcomputerchoice();
+
+playround(player_selection,computer_selection);
